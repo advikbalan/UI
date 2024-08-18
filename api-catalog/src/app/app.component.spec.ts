fd78e10573450e48e65e5a9924ff7e1e51e1f308
Mano balan <advik.balan@hotmail.com>
2024-08-18 10:02:58 +0530

    Edited index file

--- api-catalog/src/app/app.component.spec.ts
+++ api-catalog/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularProject1'`, () => {
+  it(`should have as title 'Api-catalog'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularProject1');
+    expect(app.title).toEqual('Api-catalog');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularProject1');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Api-catalog');
   });
 });
